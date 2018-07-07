6bb49fc665019f633da222fce5b5a7bc957cd384
Patricio Andres Lerzundi Suazo <patriciolerzundi@gmail.com>
2018-07-07 17:47:52 -0400

    Limpieza de texto
    Limpiando el archivo.txt para su futuro uso en el chatbot

--- chatbot.py
+++ chatbot.py
@@ -20,6 +20,36 @@ conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ig
 ## crear un diccionario para cada mapeo de las ids ##
 id2line = {}
 for line in lines:
-    _line =line.split('+++$+++')
+    _line = line.split(' +++$+++ ')
     if len(_line) == 5:
         id2line[_line[0]] = _line[4]
+        
+## crear una lista de todas las conversiones de id ##
+conversations_ids = []
+for conversation in conversations[:-1]:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
+
+questions = []
+answers = []
+for conversation in conversations_ids:
+    for i in range(len(conversation)-1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
+
+## hacer la primera limpieza en el texto ##
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm","i am",text)
+    text = re.sub(r"he's","he is",text)
+    text = re.sub(r"she's","she is",text)
+    text = re.sub(r"that's","that is",text)
+    text = re.sub(r"what's","what is",text)
+    text = re.sub(r"where's","where is",text)
+    text = re.sub(r"\'ll"," will",text)
+    text = re.sub(r"\'ve"," have",text)
+    text = re.sub(r"\'re"," are",text)
+    text = re.sub(r"\'d"," would",text)
+    text = re.sub(r"\wont't","will not",text)
+    text = re.sub(r"\cant't","cannot",text)
